51ab59933d61f7cc5294d5c22f934c80af090d28
jeffersonandrade <jefferson@voxtecnologia.com.br>
2019-10-25 15:34:07 -0300

    [ADD] - App Receita NGRX

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Store } from '@ngrx/store';
+
+import { Observable } from 'rxjs';
+
+import { LoggingService } from '../logging.service';
+import { Ingredient } from '../shared/ingredient.model';
+import * as fromApp from '../store/app.reducer';
+
+import * as ShoppingListActions from './store/shopping-list.actions';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+  ingredients: Observable<{ ingredients: Ingredient[] }>;
+
+  constructor(
+    private loggingService: LoggingService,
+    private store: Store<fromApp.AppState>
+  ) {}
+
+  ngOnInit() {
+    this.ingredients = this.store.select('shoppingList');
+    this.loggingService.printLog('Hello from ShoppingListComponent ngOnInit!');
+  }
+
+  onEditItem(index: number) {
+    this.store.dispatch(new ShoppingListActions.StartEdit(index));
+  }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  firebaseAPIKey: 'AIzaSyBKyiCg0xEhNe5TZHYBD8ZuB712x7_xvbw'
+};
